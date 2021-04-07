7e2de6b8cac5fa01ed538013c557b522d029c491
mhussainphys <mhussainphys@uchicago.edu>
2021-04-07 12:02:22 -0700

    daisy new branch3

---
+++ source/moc_dbcontrol.cpp
@@ -0,0 +1,142 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'dbcontrol.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.3)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "dbcontrol.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'dbcontrol.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.12.3. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_DBControl_t {
+    QByteArrayData data[22];
+    char stringdata0[139];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_DBControl_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_DBControl_t qt_meta_stringdata_DBControl = {
+    {
+QT_MOC_LITERAL(0, 0, 9), // "DBControl"
+QT_MOC_LITERAL(1, 10, 13), // "SaveAndValues"
+QT_MOC_LITERAL(2, 24, 0), // ""
+QT_MOC_LITERAL(3, 25, 4), // "and1"
+QT_MOC_LITERAL(4, 30, 4), // "and2"
+QT_MOC_LITERAL(5, 35, 4), // "and3"
+QT_MOC_LITERAL(6, 40, 6), // "orgate"
+QT_MOC_LITERAL(7, 47, 4), // "bsm1"
+QT_MOC_LITERAL(8, 52, 4), // "bsm2"
+QT_MOC_LITERAL(9, 57, 7), // "andTime"
+QT_MOC_LITERAL(10, 65, 9), // "delayline"
+QT_MOC_LITERAL(11, 75, 14), // "SaveRateValues"
+QT_MOC_LITERAL(12, 90, 3), // "Ra1"
+QT_MOC_LITERAL(13, 94, 3), // "Ra2"
+QT_MOC_LITERAL(14, 98, 3), // "Ra3"
+QT_MOC_LITERAL(15, 102, 3), // "Rb1"
+QT_MOC_LITERAL(16, 106, 3), // "Rb2"
+QT_MOC_LITERAL(17, 110, 3), // "Rb3"
+QT_MOC_LITERAL(18, 114, 3), // "Rc1"
+QT_MOC_LITERAL(19, 118, 3), // "Rc2"
+QT_MOC_LITERAL(20, 122, 3), // "Rc3"
+QT_MOC_LITERAL(21, 126, 12) // "hist_adqtime"
+
+    },
+    "DBControl\0SaveAndValues\0\0and1\0and2\0"
+    "and3\0orgate\0bsm1\0bsm2\0andTime\0delayline\0"
+    "SaveRateValues\0Ra1\0Ra2\0Ra3\0Rb1\0Rb2\0"
+    "Rb3\0Rc1\0Rc2\0Rc3\0hist_adqtime"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_DBControl[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       2,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    8,   24,    2, 0x0a /* Public */,
+      11,   10,   41,    2, 0x0a /* Public */,
+
+ // slots: parameters
+    QMetaType::Void, QMetaType::Int, QMetaType::Int, QMetaType::Int, QMetaType::Int, QMetaType::Int, QMetaType::Int, QMetaType::Float, QMetaType::Int,    3,    4,    5,    6,    7,    8,    9,   10,
+    QMetaType::Void, QMetaType::Int, QMetaType::Int, QMetaType::Int, QMetaType::Int, QMetaType::Int, QMetaType::Int, QMetaType::Int, QMetaType::Int, QMetaType::Int, QMetaType::Float,   12,   13,   14,   15,   16,   17,   18,   19,   20,   21,
+
+       0        // eod
+};
+
+void DBControl::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<DBControl *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->SaveAndValues((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2])),(*reinterpret_cast< int(*)>(_a[3])),(*reinterpret_cast< int(*)>(_a[4])),(*reinterpret_cast< int(*)>(_a[5])),(*reinterpret_cast< int(*)>(_a[6])),(*reinterpret_cast< float(*)>(_a[7])),(*reinterpret_cast< int(*)>(_a[8]))); break;
+        case 1: _t->SaveRateValues((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2])),(*reinterpret_cast< int(*)>(_a[3])),(*reinterpret_cast< int(*)>(_a[4])),(*reinterpret_cast< int(*)>(_a[5])),(*reinterpret_cast< int(*)>(_a[6])),(*reinterpret_cast< int(*)>(_a[7])),(*reinterpret_cast< int(*)>(_a[8])),(*reinterpret_cast< int(*)>(_a[9])),(*reinterpret_cast< float(*)>(_a[10]))); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject DBControl::staticMetaObject = { {
+    &QThread::staticMetaObject,
+    qt_meta_stringdata_DBControl.data,
+    qt_meta_data_DBControl,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *DBControl::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *DBControl::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_DBControl.stringdata0))
+        return static_cast<void*>(this);
+    return QThread::qt_metacast(_clname);
+}
+
+int DBControl::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QThread::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 2)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 2;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
---
+++ source/moc_qutag_adq.cpp
@@ -0,0 +1,286 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'qutag_adq.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.3)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "qutag_adq.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'qutag_adq.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.12.3. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_qutagadq_t {
+    QByteArrayData data[42];
+    char stringdata0[534];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_qutagadq_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_qutagadq_t qt_meta_stringdata_qutagadq = {
+    {
+QT_MOC_LITERAL(0, 0, 8), // "qutagadq"
+QT_MOC_LITERAL(1, 9, 9), // "dataready"
+QT_MOC_LITERAL(2, 19, 0), // ""
+QT_MOC_LITERAL(3, 20, 11), // "vectorInt64"
+QT_MOC_LITERAL(4, 32, 14), // "vectorTimetags"
+QT_MOC_LITERAL(5, 47, 10), // "vectorInt8"
+QT_MOC_LITERAL(6, 58, 14), // "vectorChannels"
+QT_MOC_LITERAL(7, 73, 7), // "tsvalid"
+QT_MOC_LITERAL(8, 81, 9), // "qutaghist"
+QT_MOC_LITERAL(9, 91, 12), // "vectorDouble"
+QT_MOC_LITERAL(10, 104, 7), // "TTdata1"
+QT_MOC_LITERAL(11, 112, 7), // "TTdata2"
+QT_MOC_LITERAL(12, 120, 7), // "TTdata3"
+QT_MOC_LITERAL(13, 128, 5), // "adqui"
+QT_MOC_LITERAL(14, 134, 13), // "setHistograms"
+QT_MOC_LITERAL(15, 148, 14), // "adqpausechange"
+QT_MOC_LITERAL(16, 163, 5), // "chang"
+QT_MOC_LITERAL(17, 169, 5), // "Break"
+QT_MOC_LITERAL(18, 175, 9), // "filterset"
+QT_MOC_LITERAL(19, 185, 19), // "Chang_in_binsinplot"
+QT_MOC_LITERAL(20, 205, 3), // "val"
+QT_MOC_LITERAL(21, 209, 18), // "Chang_in_histStart"
+QT_MOC_LITERAL(22, 228, 16), // "Chang_in_histEnd"
+QT_MOC_LITERAL(23, 245, 18), // "Chang_in_startChan"
+QT_MOC_LITERAL(24, 264, 16), // "Chang_in_adqtime"
+QT_MOC_LITERAL(25, 281, 18), // "Chang_in_PlotAChn1"
+QT_MOC_LITERAL(26, 300, 18), // "Chang_in_PlotAChn2"
+QT_MOC_LITERAL(27, 319, 18), // "Chang_in_PlotBChn1"
+QT_MOC_LITERAL(28, 338, 18), // "Chang_in_PlotBChn2"
+QT_MOC_LITERAL(29, 357, 18), // "Chang_in_PlotCChn1"
+QT_MOC_LITERAL(30, 376, 18), // "Chang_in_PlotCChn2"
+QT_MOC_LITERAL(31, 395, 17), // "Chang_anlAvilable"
+QT_MOC_LITERAL(32, 413, 14), // "Chang_in_thch1"
+QT_MOC_LITERAL(33, 428, 14), // "Chang_in_thch2"
+QT_MOC_LITERAL(34, 443, 14), // "Chang_in_thch3"
+QT_MOC_LITERAL(35, 458, 14), // "Chang_in_thch4"
+QT_MOC_LITERAL(36, 473, 11), // "Chang_in_cw"
+QT_MOC_LITERAL(37, 485, 10), // "Chang_rof1"
+QT_MOC_LITERAL(38, 496, 4), // "text"
+QT_MOC_LITERAL(39, 501, 10), // "Chang_rof2"
+QT_MOC_LITERAL(40, 512, 10), // "Chang_rof3"
+QT_MOC_LITERAL(41, 523, 10) // "Chang_rof4"
+
+    },
+    "qutagadq\0dataready\0\0vectorInt64\0"
+    "vectorTimetags\0vectorInt8\0vectorChannels\0"
+    "tsvalid\0qutaghist\0vectorDouble\0TTdata1\0"
+    "TTdata2\0TTdata3\0adqui\0setHistograms\0"
+    "adqpausechange\0chang\0Break\0filterset\0"
+    "Chang_in_binsinplot\0val\0Chang_in_histStart\0"
+    "Chang_in_histEnd\0Chang_in_startChan\0"
+    "Chang_in_adqtime\0Chang_in_PlotAChn1\0"
+    "Chang_in_PlotAChn2\0Chang_in_PlotBChn1\0"
+    "Chang_in_PlotBChn2\0Chang_in_PlotCChn1\0"
+    "Chang_in_PlotCChn2\0Chang_anlAvilable\0"
+    "Chang_in_thch1\0Chang_in_thch2\0"
+    "Chang_in_thch3\0Chang_in_thch4\0Chang_in_cw\0"
+    "Chang_rof1\0text\0Chang_rof2\0Chang_rof3\0"
+    "Chang_rof4"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_qutagadq[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      28,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       2,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    3,  154,    2, 0x06 /* Public */,
+       8,    3,  161,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+      13,    0,  168,    2, 0x0a /* Public */,
+      14,    0,  169,    2, 0x0a /* Public */,
+      15,    1,  170,    2, 0x0a /* Public */,
+      17,    0,  173,    2, 0x0a /* Public */,
+      18,    0,  174,    2, 0x0a /* Public */,
+      19,    1,  175,    2, 0x0a /* Public */,
+      21,    1,  178,    2, 0x0a /* Public */,
+      22,    1,  181,    2, 0x0a /* Public */,
+      23,    1,  184,    2, 0x0a /* Public */,
+      24,    1,  187,    2, 0x0a /* Public */,
+      25,    1,  190,    2, 0x0a /* Public */,
+      26,    1,  193,    2, 0x0a /* Public */,
+      27,    1,  196,    2, 0x0a /* Public */,
+      28,    1,  199,    2, 0x0a /* Public */,
+      29,    1,  202,    2, 0x0a /* Public */,
+      30,    1,  205,    2, 0x0a /* Public */,
+      31,    1,  208,    2, 0x0a /* Public */,
+      32,    1,  211,    2, 0x0a /* Public */,
+      33,    1,  214,    2, 0x0a /* Public */,
+      34,    1,  217,    2, 0x0a /* Public */,
+      35,    1,  220,    2, 0x0a /* Public */,
+      36,    1,  223,    2, 0x0a /* Public */,
+      37,    1,  226,    2, 0x0a /* Public */,
+      39,    1,  229,    2, 0x0a /* Public */,
+      40,    1,  232,    2, 0x0a /* Public */,
+      41,    1,  235,    2, 0x0a /* Public */,
+
+ // signals: parameters
+    QMetaType::Void, 0x80000000 | 3, 0x80000000 | 5, QMetaType::Int,    4,    6,    7,
+    QMetaType::Void, 0x80000000 | 9, 0x80000000 | 9, 0x80000000 | 9,   10,   11,   12,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Bool,   16,
+    QMetaType::Void,
+    QMetaType::Int,
+    QMetaType::Void, QMetaType::Int,   20,
+    QMetaType::Void, QMetaType::Int,   20,
+    QMetaType::Void, QMetaType::Int,   20,
+    QMetaType::Void, QMetaType::Int,   20,
+    QMetaType::Void, QMetaType::Double,   20,
+    QMetaType::Void, QMetaType::Int,   20,
+    QMetaType::Void, QMetaType::Int,   20,
+    QMetaType::Void, QMetaType::Int,   20,
+    QMetaType::Void, QMetaType::Int,   20,
+    QMetaType::Void, QMetaType::Int,   20,
+    QMetaType::Void, QMetaType::Int,   20,
+    QMetaType::Void, QMetaType::Bool,   20,
+    QMetaType::Void, QMetaType::Double,   20,
+    QMetaType::Void, QMetaType::Double,   20,
+    QMetaType::Void, QMetaType::Double,   20,
+    QMetaType::Void, QMetaType::Double,   20,
+    QMetaType::Void, QMetaType::Int,   20,
+    QMetaType::Void, QMetaType::QString,   38,
+    QMetaType::Void, QMetaType::QString,   38,
+    QMetaType::Void, QMetaType::QString,   38,
+    QMetaType::Void, QMetaType::QString,   38,
+
+       0        // eod
+};
+
+void qutagadq::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<qutagadq *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->dataready((*reinterpret_cast< const vectorInt64(*)>(_a[1])),(*reinterpret_cast< const vectorInt8(*)>(_a[2])),(*reinterpret_cast< int(*)>(_a[3]))); break;
+        case 1: _t->qutaghist((*reinterpret_cast< const vectorDouble(*)>(_a[1])),(*reinterpret_cast< const vectorDouble(*)>(_a[2])),(*reinterpret_cast< const vectorDouble(*)>(_a[3]))); break;
+        case 2: _t->adqui(); break;
+        case 3: _t->setHistograms(); break;
+        case 4: _t->adqpausechange((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 5: _t->Break(); break;
+        case 6: { int _r = _t->filterset();
+            if (_a[0]) *reinterpret_cast< int*>(_a[0]) = std::move(_r); }  break;
+        case 7: _t->Chang_in_binsinplot((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 8: _t->Chang_in_histStart((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 9: _t->Chang_in_histEnd((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 10: _t->Chang_in_startChan((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 11: _t->Chang_in_adqtime((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 12: _t->Chang_in_PlotAChn1((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 13: _t->Chang_in_PlotAChn2((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 14: _t->Chang_in_PlotBChn1((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 15: _t->Chang_in_PlotBChn2((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 16: _t->Chang_in_PlotCChn1((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 17: _t->Chang_in_PlotCChn2((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 18: _t->Chang_anlAvilable((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 19: _t->Chang_in_thch1((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 20: _t->Chang_in_thch2((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 21: _t->Chang_in_thch3((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 22: _t->Chang_in_thch4((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 23: _t->Chang_in_cw((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 24: _t->Chang_rof1((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 25: _t->Chang_rof2((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 26: _t->Chang_rof3((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 27: _t->Chang_rof4((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (qutagadq::*)(const vectorInt64 & , const vectorInt8 & , int );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&qutagadq::dataready)) {
+                *result = 0;
+                return;
+            }
+        }
+        {
+            using _t = void (qutagadq::*)(const vectorDouble & , const vectorDouble & , const vectorDouble & );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&qutagadq::qutaghist)) {
+                *result = 1;
+                return;
+            }
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject qutagadq::staticMetaObject = { {
+    &QThread::staticMetaObject,
+    qt_meta_stringdata_qutagadq.data,
+    qt_meta_data_qutagadq,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *qutagadq::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *qutagadq::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_qutagadq.stringdata0))
+        return static_cast<void*>(this);
+    return QThread::qt_metacast(_clname);
+}
+
+int qutagadq::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QThread::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 28)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 28;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 28)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 28;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void qutagadq::dataready(const vectorInt64 & _t1, const vectorInt8 & _t2, int _t3)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)), const_cast<void*>(reinterpret_cast<const void*>(&_t2)), const_cast<void*>(reinterpret_cast<const void*>(&_t3)) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+
+// SIGNAL 1
+void qutagadq::qutaghist(const vectorDouble & _t1, const vectorDouble & _t2, const vectorDouble & _t3)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)), const_cast<void*>(reinterpret_cast<const void*>(&_t2)), const_cast<void*>(reinterpret_cast<const void*>(&_t3)) };
+    QMetaObject::activate(this, &staticMetaObject, 1, _a);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
